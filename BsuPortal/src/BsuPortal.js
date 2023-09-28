d7d1a063f818ff2ba26f050e067b5af2d2ce41b2
Jaba98 <jabasharadzej@gmail.com>
2023-09-28 19:39:38 +0400

    Added new tab feature

---
+++ BsuPortal/src/BsuPortal.js
@@ -0,0 +1,345 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { AppRegistry,StyleSheet,View,StatusBar,BackHandler,Text,Alert,Linking,SafeAreaView} from 'react-native';
+import { WebView} from 'react-native-webview';
+import SplashScreen from './SplashScreen';
+import NetInfo from '@react-native-community/netinfo';
+import RNFetchBlob from 'rn-fetch-blob';
+import RNFS from 'react-native-fs';
+import PushNotification from 'react-native-push-notification'; // for Android and iOS
+import { ToastAndroid } from 'react-native';
+import { PermissionsAndroid, Platform} from 'react-native';
+import { useNavigation,useRoute, } from '@react-navigation/native';
+
+
+const BsuPortal = () => {
+  const [showSplashScreen, setShowSplashScreen] = useState(true); 
+  const webViewRef = useRef(null);
+  const [newUrl, setNewUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [isConnected, setIsConnected] = useState(true);
+  const [showHeader, setShowHeader] = useState(false);
+  const [redirectedUrl, setRedirectedUrl] = useState('');
+  const [originalUrl, setOriginalUrl] = useState('https://portal.bsu.edu.ge/'); // Track the original URL
+  const navigation = useNavigation(); // Use useNavigation inside the functional component
+  const route = useRoute(); // Use useRoute to access route params
+
+
+  // --- useEffect ტექნიკის დაბრუნების ღილაკის დასამუშავებლად---
+  useEffect(() => {
+
+    // უკანა ღილაკის დაჭერის ფუნქცია
+    const backAction = () => {
+      if (webViewRef.current) {
+        webViewRef.current.goBack();
+        console.log('Hardware back button pressed. Navigating back in WebView.');
+        return true;
+      }
+      return false;
+    };
+
+    // მოვლენის მსმენელის დამატება აპარატურის უკან ღილაკისთვის
+    BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    // 3 წამის შემდეგ დამალეთ დახრილი ეკრანი
+    setTimeout(() => {
+      console.log('Splash screen hidden after 3 seconds.');
+      setShowSplashScreen(false);
+    }, 3000);
+
+    // გასუფთავება: ამოიღეთ მოვლენის მსმენელი კომპონენტის დემონტაჟის დროს
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', backAction);
+    };
+  }, []);
+
+  // --- useEffect ინტერნეტ კავშირის მონიტორინგისთვის ---
+  useEffect(() => {
+    // გამოიწერეთ ქსელის მდგომარეობის ცვლილებები
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      console.log('Internet connectivity status changed:', state.isConnected);
+      setIsConnected(state.isConnected);
+
+      if (!state.isConnected) {
+        console.log('No Internet. Showing error message.');
+        Alert.alert(
+          'No Internet Connection',
+          'Please check your internet connection and try again.',
+          [
+            {
+              text: 'Refresh',
+              onPress: () => {
+                webViewRef.current.reload();
+              },
+            },
+          ]
+        );
+      }
+    });
+
+    // გასუფთავება: გამოწერის გაუქმება კომპონენტის დამონტაჟებისას
+    return () => {
+      unsubscribe();
+    };
+    
+  }, []);
+
+        // --- handleWebViewNavigation ფუნქცია WebView ნავიგაციისთვის --
+        const handleWebViewNavigation = (event) => {
+             const { url, navigationType } = event;
+             console.log(`URL: ${url}, Navigation Type: ${navigationType}`);
+
+        }
+
+        // PushNotification.localNotification({
+        //  title: 'Download failed',
+        //  message: 'File download failed', // Customize as needed
+        //  smallIcon: 'ic_launcher',
+        //  visibility: 'public',
+        //  priority: 'high',
+        //  importance: 'high',
+        //  channelId: 'download-channel',
+        //  number: 0,
+        //  id: notificationId,
+        //  });
+        
+        // Updated handleFileDownload function to handle file downloads
+     
+        const handleFileDownload = async (url) => {
+          try {
+            const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+            );
+      
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+              const downloadDir = RNFS.DownloadDirectoryPath;
+              ToastAndroid.show('მიმდინარეობს ჩამოტვირთვა...', ToastAndroid.SHORT);
+      
+              const response = await RNFetchBlob.config({
+                path: `${downloadDir}/filename.extension`,
+                overwrite: true,
+                notification: true,
+              }).fetch('GET', url);
+      
+              const status = response.info().status;
+              console.log('Response Status:', status);
+      
+              ToastAndroid.show('ჩამოტვირთვა დასრულდა!', ToastAndroid.SHORT);
+      
+              if (status === 200) {
+                const contentDisposition = response.info().headers['Content-Disposition'];
+      
+                if (contentDisposition) {
+                  const filename = contentDisposition
+                    .split(';')[1]
+                    .trim()
+                    .split('=')[1]
+                    .replace(/"/g, '');
+      
+                  const filePath = `${downloadDir}/${filename}`;
+      
+                  await RNFS.moveFile(response.path(), filePath);
+      
+                  console.log('File Downloaded. Path:', filePath);
+      
+                  const exists = await RNFS.exists(filePath);
+                  if (exists) {
+                    console.log('File exists at the specified path.');
+                  } else {
+                    console.error('File does not exist at the specified path.');
+                  }
+                } else {
+                  console.error('Content-Disposition header not found.');
+                }
+              } else {
+                console.error('HTTP Error:', status);
+      
+                if (status === 404) {
+                  console.error('File not found.');
+                } else if (status === 403) {
+                  console.error('Access denied.');
+                } else {
+                  console.error('Unhandled HTTP status code.');
+                }
+              }
+            } else {
+              console.error('Permission denied for file download.');
+            }
+          } catch (error) {
+            console.error('File download error:', error);
+          }
+      };
+
+
+    //  const headerNavigation = (event) => {
+    //    const { url, navigationType } = event;
+    //    console.log(`URL: ${url}, Navigation Type: ${navigationType}`);
+    //  
+    //    // Remove "https://" prefix from the URL
+    //    const urlWithoutHttps = url.replace(/^https:\/\/(www\.)?/, '');
+    //    // Set the redirected URL
+    //    setRedirectedUrl(urlWithoutHttps);
+    //    // Check if the URL starts with 'https://portal' to hide/show the header
+    //    if (url.startsWith('https://portal')) {
+    //      setShowHeader(false);
+    //    } else {
+    //      setShowHeader(true);
+    //    }
+    //  }
+   return (
+    <SafeAreaView style={styles.container}>
+            <StatusBar backgroundColor="#03a9f3" barStyle="dark-content" />
+
+      {showSplashScreen ? (
+        <View style={styles.splashContainer}>
+          <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
+          <SplashScreen />
+        </View>
+      ) : (
+        <>
+          {!isConnected && (
+            <View style={styles.internetMessageContainer}>
+              <Text style={styles.internetMessageText}>Please turn on the Internet.</Text>
+            </View>
+          )}
+
+          <WebView
+             ref={webViewRef}
+             // მიუთითეთ წყაროს URL WebView-ისთვის, გამოიყენეთ „newUrl“, თუ ეს შესაძლებელია, ან ნაგულისხმევი URL
+             source={{ uri: originalUrl }} // Use the current URL in the WebView source
+             style={styles.webView}
+             // ჩართეთ გადახვევა WebView-ში
+             scrollEnabled={true}
+             // ჩართეთ JavaScript-ის შესრულება WebView-ში
+             javaScriptEnabled={true}
+             // გამოძახება „ჩატვირთვის“ მდგომარეობის დასაყენებლად ჭეშმარიტად, როდესაც WebView ჩატვირთვას დაიწყებს
+             onLoadStart={() => setLoading(true)}
+             // გამოძახება „ჩატვირთვის“ მდგომარეობის დასაყენებლად false-ზე, როდესაც WebView დაასრულებს ჩატვირთვას
+             onLoadEnd={() => setLoading(false)}
+             // გამოძახება „ჩატვირთვის“ მდგომარეობის დასაყენებლად false-ზე, როდესაც WebView დაასრულებს ჩატვირთვას
+             onLoad={() => {
+              setLoading(true); // Hide loading indicator
+            }}
+             // გამოიყენეთ WKWebView ძრავა iOS-ზე გაუმჯობესებული შესრულებისა და თანმიმდევრულობისთვის
+             useWebKit={true}
+             // გამორთეთ შინაარსის ავტომატური მასშტაბირება ეკრანზე მორგებისთვის
+             scalesPageToFit={true}
+             // დააყენეთ მომხმარებლის აგენტის სათაური WebView-ისთვის
+             userAgent={
+               'Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'
+             }
+             // გამორთეთ მხარდაჭერა მრავალი ფანჯრის ან ჩანართისთვის WebView-ში
+             setSupportMultipleWindows={false}
+             // ჩართეთ გაზიარებული ქუქიები მომხმარებლის სესიების შესანარჩუნებლად WebView-სა და აპს შორის
+             sharedCookiesEnabled={true}
+             // შერეული კონტენტის ჩატვირთვის დაშვება (HTTP კონტენტი HTTPS გვერდზე)
+             mixedContentMode="always"
+             // შეიტანეთ მორგებული JavaScript კოდი WebView-ში ხედვის პორტის მეტა ტეგის დასაყენებლად
+             injectedJavaScript={`
+                  const meta = document.createElement('meta');
+                  meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+                  meta.setAttribute('name', 'viewport');
+                  document.getElementsByTagName('head')[0].appendChild(meta);'});
+                  });
+                `}
+
+                onNavigationStateChange={(navState) => {
+                  handleWebViewNavigation(navState);
+                  //headerNavigation(navState);
+                }}
+                
+                
+              onShouldStartLoadWithRequest={(event) => {
+                const { url } = event;
+            
+                 // Check if the URL starts with 'https://portal.bsu'
+                 if (!url.startsWith('https://portal.bsu')) {
+                   console.log('Blocked navigation to URL:', url);
+                  // Check if the URL starts with 'https://portal.bsu'
+                   navigation.navigate('იტვირთება...', { redirectedUrl: url });
+                   console.log('open NewTab');
+                   return false; // Block navigation for URLs that do not start with 'https://portal.bsu'
+                 }
+
+                // Check if the URL contains the download.php key
+                if (url.includes('Download.php?Key=')) {
+                  // Handle the file download
+                  handleFileDownload(url);
+                  return false; // Return false to cancel the WebView navigation
+                }
+                // Check if the URL ends with a common file extension (e.g., PDF, Excel, Word)
+                const fileExtensions = ['.pdf', '.xlsx', '.xls', '.doc', '.docx'];
+                const lowercaseUrl = url.toLowerCase();
+                const hasValidExtension = fileExtensions.some(extension => lowercaseUrl.endsWith(extension));
+                
+                if (hasValidExtension) {
+                  // Handle the file download
+                  handleFileDownload(url);
+                  return false; // Return false to cancel the WebView navigation
+                }
+
+                // Allow other requests to load
+                return true;
+              }}
+
+               /> 
+             </>
+           )}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  downloadingContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  },
+  downloadingText: {
+    color: '#000000', // Change to the desired text color
+    fontSize: 18, // Adjust the font size as needed
+  },
+
+  splashContainer: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  webView: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+   redirectedUrlText: {
+    color: '#03a9f3', // Change 'red' to the desired color
+    fontSize: 16, // Adjust the font size as needed
+  },
+
+  internetMessageContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  },
+  internetMessageText: {
+    color: '#000000',
+    fontSize: 18,
+  },
+
+});
+
+// დაარეგისტრირეთ კომპონენტი AppRegistry-ით
+AppRegistry.registerComponent('BsuPortal', () => BsuPortal);
+
+// კომპონენტის ექსპორტი, როგორც ნაგულისხმევი ექსპორტი
+ export default BsuPortal;
--- BsuPortal/src/NewTab.js
+++ BsuPortal/src/NewTab.js
@@ -1,35 +1,130 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useRef,useState } from 'react';
+import { View, StyleSheet,BackHandler,Text} from 'react-native';
+import { WebView } from 'react-native-webview'
 
-const NewTab = ({ url }) => {
-  // Customize the design and content for the different URL
+const NewTab = ({ route,navigation }) => {
+  // Get the redirected URL from the route parameters
+  const { redirectedUrl } = route.params;
+  const webViewRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+   // Function to extract the domain from a URL
+   const extractDomain = (url) => {
+    let domain = url;
+    // Find & remove protocol (http, ftp, etc.) and get the domain
+    if (url.indexOf('://') > -1) {
+      domain = url.split('/')[2];
+    } else {
+      domain = url.split('/')[0];
+    }
+    // Find & remove port number
+    domain = domain.split(':')[0];
+    // Remove "www" if it exists at the beginning of the domain
+    if (domain.startsWith('www.')) {
+      domain = domain.substring(4);
+    }
+    return domain;
+  };
+
+  const backAction = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      console.log('Hardware back button pressed. Navigating back in WebView.');
+      return true; // Return true to indicate that the back button press is handled
+    }
+    return false; // Return false if the WebView cannot go back
+  };
+
+     // --- useEffect ტექნიკის დაბრუნების ღილაკის დასამუშავებლად---
+     useEffect(() => {
+      BackHandler.addEventListener('hardwareBackPress', backAction);
+    
+      return () => {
+        BackHandler.removeEventListener('hardwareBackPress', backAction);
+      };
+    }, []);
+
+    // --- handleWebViewNavigation ფუნქცია WebView ნავიგაციისთვის ---
+      const handleWebViewNavigation = (event) => {
+        const { url, navigationType, loading } = event;
+    
+        if (loading) {
+          // The WebView is currently loading a new page
+          setIsLoading(true); // Set loading state to true when a new page starts loading
+        } else {
+          // The WebView has finished loading
+          setIsLoading(false); // Set loading state to false when the page has finished loading
+        }
+
+        // განახორციელეთ HTTPS, თუ URL უკვე არ იწყება მისით
+         if (!url.startsWith('https://')) {
+          console.log('Insecure connection detected. Redirecting to HTTPS.');
+          const secureUrl = 'https://' + url.replace(/^https?:\/\//i, '');
+          webViewRef.current.injectJavaScript(`
+            window.location.href = "${secureUrl}";
+          `);
+          return false;
+        }
+
+        return true;
+      };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Custom Web Page</Text>
-      <Text style={styles.url}>URL: {url}</Text>
-      {/* Add your custom content here */}
+      
+      <WebView
+        ref={webViewRef}
+        source={{ uri: redirectedUrl }}
+        style={styles.webView}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={isLoading} // Set startInLoadingState based on isLoading state
+        setSupportMultipleWindows={false}
+        mixedContentMode="always"
+        useWebKit={true}
+        userAgent={
+          'Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'
+        }
+        injectedJavaScript={`
+        const meta = document.createElement('meta');
+        meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+        meta.setAttribute('name', 'viewport');
+        document.getElementsByTagName('head')[0].appendChild(meta);'});
+        });
+      `}
+        onNavigationStateChange={(navState) => {
+            // Extract the domain from the URL using the extractDomain function
+          const domain = extractDomain(navState.url);
+
+          // Render a custom header title with yellow text color
+          navigation.setOptions({
+            headerTitle: () => (
+              <Text style={{ color: '#03a9f3', fontSize: 18, textAlign: 'center' }}>
+                {domain || redirectedUrl}
+              </Text>
+            ),
+          });
+          handleWebViewNavigation(navState);
+        }}
+                        
+      />
     </View>
   );
 };
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
   },
-  header: {
-    fontSize: 24,
+  title: {
+    fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 16,
+    margin: 10,
+    
   },
-  url: {
-    fontSize: 16,
-    color: 'blue',
-    marginBottom: 16,
+  webView: {
+    flex: 1,
   },
-  // Add more styles for your custom content
-};
+});
 
 export default NewTab;
